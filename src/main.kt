ff619cfc659213a3e24bd38f10bbbcca3c109bc1
jmcmahon38 <jmcmahon38@students.mchenry.edu>
2021-09-27 11:28:17 -0500

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,10 @@
+fun main() {
+
+    val favoriteThings = arrayOf(
+        "Raindrops on roses", "Whiskers on kittens", "Bright copper kettles",
+        "Warm woolen mittens", "Brown paper packages tied up with strings")
+
+    for (item in favoriteThings){
+        println(item)
+    }
+}
